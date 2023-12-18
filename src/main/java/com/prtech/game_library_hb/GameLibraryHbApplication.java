332ced5b333fe5b299af64c6e12dc554dc7834e3
Przemyslaw <przemek.rzymkowski@gmail.com>
2023-12-18 10:17:20 +0100

    Initial commit

---
+++ src/main/java/com/prtech/game_library_hb/GameLibraryHbApplication.java
@@ -0,0 +1,13 @@
+package com.prtech.game_library_hb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLibraryHbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLibraryHbApplication.class, args);
+	}
+
+}
