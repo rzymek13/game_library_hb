c2ae8fb217833450dcde2abe6d1b9f226bef82d6
przymkowski <przemek.rzymkowski@gmail.com>
2024-01-02 21:47:21 +0100

    some fixes

--- src/main/java/com/prtech/game_library_hb/controller/TeamController.java
+++ src/main/java/com/prtech/game_library_hb/controller/TeamController.java
@@ -1,12 +1,14 @@
 package com.prtech.game_library_hb.controller;
 
 import com.prtech.game_library_hb.repository.TeamRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
-@RestController
-@RequestMapping("/teams")
+@RepositoryRestController
+@Slf4j
 public class TeamController {
     private final TeamRepository teamRepository;
 
@@ -14,7 +16,9 @@ public class TeamController {
         this.teamRepository = teamRepository;
     }
 
-//    public List<Team> findAll() {
-//        return teamRepository.findAll();
-//    }
+    @GetMapping(value = "/teams", params = {"!sort", "!page", "!size"})
+    ResponseEntity<?> readAllTeams() {
+        log.info("All the teams");
+        return ResponseEntity.ok(teamRepository.findAll());
+    }
 }
--- src/main/java/com/prtech/game_library_hb/repository/TeamRepository.java
+++ src/main/java/com/prtech/game_library_hb/repository/TeamRepository.java
@@ -4,9 +4,18 @@ import com.prtech.game_library_hb.model.Team;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 
 @RepositoryRestResource
 public interface TeamRepository extends JpaRepository<Team, Integer> {
+
+    @Override
+    @RestResource
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource
+    void delete(Team team);
 
 }
